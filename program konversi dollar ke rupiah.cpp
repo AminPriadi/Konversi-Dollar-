1300cb90c8290e0d2b9bfb34c2d59eb274d57070
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 11:03:30 +0700

    Add files via upload

---
+++ program konversi dollar ke rupiah.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float rupiah_per_dollar=15170;
+    int dollar,rupiah;
+    cout<<"masukan berapa dollar yang akan anda hitung ";
+    cin>>dollar;
+
+    rupiah=dollar*15170;
+    cout<<"maka harga dollar adalah ";
+    cout<<rupiah;
+
+    return 0;
+}
